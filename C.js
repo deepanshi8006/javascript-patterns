fd7ecff69648a25aaac7d527b0c68298c806ac94
deepanshi8006 <mishradeepanshi8006@gmail.com>
2025-12-04 10:35:54 +0530

    pattern of c

---
+++ C.js
@@ -0,0 +1,15 @@
+let n=5;
+ let top="";
+ for(let j=0; j<n; j++){
+    top+="* ";
+ }
+ console.log(top);
+for(let i=0; i<n-2; i++){
+    console.log("*");
+    }
+let bottom="";
+ for(let j=0; j<n; j++){
+    bottom+="* ";
+ }
+  console.log(bottom);
+
